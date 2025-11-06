2727cdb53a2577a01b296880a87892b13732a0ea
Gadiel Ocaña <oca231270@uvg.edu.gt>
2025-11-05 23:52:17 -0600

    Versión terminada - ajustes de calidad

--- RendererOpenGL2025.py
+++ RendererOpenGL2025.py
@@ -64,7 +64,7 @@ model3 = Model("models/gremlingus.obj")  # Cambia esto por tu tercer archivo .ob
 model3.position.x = 0
 model3.position.y = -2
 model3.position.z = -12
-model3.scale = glm.vec3(0.05, 0.05, 0.05)
+model3.scale = glm.vec3(2.0, 2.0, 2.0)  # Escala grande como solicitaste
 
 # Lista de modelos y control del modelo actual
 models = [model1, model2, model3]
